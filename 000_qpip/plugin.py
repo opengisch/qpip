1fab6484f2ce235c24a4db1e7778e34314860424
olivierdalang <olivier.dalang@gmail.com>
2021-07-09 15:51:36 +0200

    avancement

--- 000_qpip/plugin.py
+++ 000_qpip/plugin.py
@@ -1,10 +1,14 @@
-import os.path
+import os
+import subprocess
+import sys
 
 import pkg_resources
+from PyQt5 import uic
 from qgis import utils
 from qgis.core import QgsApplication, QgsMessageLog
+from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction, QCheckBox, QDialog, QGridLayout, QLabel
+from qgis.PyQt.QtWidgets import QAction, QDialog, QTableWidgetItem
 
 
 class Plugin:
@@ -19,9 +23,22 @@ class Plugin:
         self.plugins_path = os.path.join(
             QgsApplication.qgisSettingsDirPath(), "python", "plugins"
         )
-        self.pip_deps_path = os.path.join(
-            QgsApplication.qgisSettingsDirPath(), "python", "pip_deps"
+        self.prefix_path = os.path.join(
+            QgsApplication.qgisSettingsDirPath().replace("/", os.path.sep),
+            "python",
+            "dependencies",
         )
+        self.site_packages_path = os.path.join(self.prefix_path, "Lib", "site-packages")
+        self.bin_path = os.path.join(self.prefix_path, "Scripts")
+
+        if self.site_packages_path not in sys.path:
+            QgsMessageLog.logMessage(
+                f"Adding {self.site_packages_path} to PYTHONPATH", "Plugins"
+            )
+            sys.path.append(self.site_packages_path)
+        if self.bin_path not in os.environ["PATH"]:
+            QgsMessageLog.logMessage(f"Adding {self.bin_path} to PATH", "Plugins")
+            os.environ["PATH"] = self.bin_path + ";" + os.environ["PATH"]
 
         # Monkey patch qgis.utils
         QgsMessageLog.logMessage("Applying monkey patch to qgis.utils", "Plugins")
@@ -59,6 +76,10 @@ class Plugin:
         QgsMessageLog.logMessage("Unapplying monkey patch to qgis.utils", "Plugins")
         utils.loadPlugin = self._initial_loadPlugin
 
+        # Remove path alterations
+        if self.site_packages_path in sys.path:
+            sys.path.remove(self.site_packages_path)
+
     def patched_load_plugin(self, packageName):
         """
         This replaces qgis.utils.loadPlugin to check if dependencies are met. If so, it just calls the
@@ -73,20 +94,19 @@ class Plugin:
         if os.path.isfile(requirements_path):
             QgsMessageLog.logMessage(f"{packageName} has a requirements.txt", "Plugins")
 
-            missing = {}
+            missing_deps = {}
 
-            requirements = pkg_resources.parse_requirements(
-                open(requirements_path, "r")
-            )
-            for requirement in requirements:
-                try:
-                    pkg_resources.require(str(requirement))
-                except pkg_resources.DistributionNotFound:
-                    missing[str(requirement)] = "missing"
-                except pkg_resources.VersionConflict:
-                    missing[str(requirement)] = "conflict"
+            with open(requirements_path, "r") as f:
+                requirements = pkg_resources.parse_requirements(f)
+                for requirement in requirements:
+                    try:
+                        pkg_resources.require(str(requirement))
+                    except pkg_resources.DistributionNotFound as e:
+                        missing_deps[str(requirement)] = "missing"
+                    except pkg_resources.VersionConflict as e:
+                        missing_deps[str(requirement)] = f"conflict ({e.dist})"
 
-            if missing:
+            if missing_deps:
                 if not self._init_complete:
                     QgsMessageLog.logMessage(
                         f"{packageName} has missing requirements. We defer loading to after initialization",
@@ -95,16 +115,42 @@ class Plugin:
                     self._defered_packages.append(packageName)
                     return False
 
-                layout = QGridLayout()
-                for req, state in missing.items():
-                    row = layout.rowCount()
-                    layout.addWidget(QLabel(req), row, 0)
-                    layout.addWidget(QLabel(state), row, 1)
-                    layout.addWidget(QCheckBox(), row, 2)
                 dialog = QDialog()
-                dialog.setLayout(layout)
+                uic.loadUi(os.path.join(os.path.dirname(__file__), "dialog.ui"), dialog)
+
+                checkboxes = {}
+                dialog.tableWidget.setRowCount(len(missing_deps))
+                for i, (req, state) in enumerate(missing_deps.items()):
+                    checkboxes[req] = QTableWidgetItem()
+                    checkboxes[req].setCheckState(Qt.Checked)
+                    dialog.tableWidget.setItem(i, 0, QTableWidgetItem(packageName))
+                    dialog.tableWidget.setItem(i, 1, QTableWidgetItem(req))
+                    dialog.tableWidget.setItem(i, 2, QTableWidgetItem(state))
+                    dialog.tableWidget.setItem(i, 3, checkboxes[req])
 
                 dialog.exec_()
+
+                deps_to_install = [
+                    req
+                    for req, checkbox in checkboxes.items()
+                    if checkbox.checkState() == Qt.Checked
+                ]
+
+                if deps_to_install:
+                    os.makedirs(self.prefix_path, exist_ok=True)
+                    pip_args = [
+                        "python",
+                        "-m",
+                        "pip",
+                        "install",
+                        *deps_to_install,
+                        "--prefix",
+                        self.prefix_path,
+                    ]
+                    QgsMessageLog.logMessage(
+                        f"Running command : {pip_args=}", "Plugins"
+                    )
+                    subprocess.check_call(pip_args, shell=True)
 
         QgsMessageLog.logMessage(f"Loading {packageName}", "Plugins")
         return self._initial_loadPlugin(packageName)
